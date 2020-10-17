4d9d87042361b5e94b81942824df1e84f21f05b0
Nick Kniola <nickkniola@me.com>
2020-10-16 19:59:43 -0500

    in progress

---
+++ array-manipulation-2/src/take.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+
+function take(array, count) {
+  var newArray = [];
+
+  /* if (array === ) */
+
+  for (let i = 0; i < count; i++) {
+    newArray.push(array[i]);
+  }
+
+  return newArray;
+}
