43b490394eff2644eb6dcf5158d711d8b01e7faa
Nick Kniola <nickkniola@me.com>
2020-10-19 11:03:07 -0700

    Completed Dom Querying

---
+++ dom-querying/main.js
@@ -0,0 +1,18 @@
+console.log('hello, world');
+var h1Element = document.querySelector('h1');
+console.log(h1Element);
+console.dir(h1Element);
+
+var idExplanationElement = document.querySelector('#explanation');
+console.log(idExplanationElement);
+console.dir(idExplanationElement);
+
+var classHintElement = document.querySelector('.hint');
+console.log(classHintElement);
+console.dir(classHintElement);
+
+var allPElements = document.querySelectorAll('p');
+console.log(allPElements);
+
+var allClassExampleLinkElements = document.querySelectorAll('.example-link');
+console.log(allClassExampleLinkElements);
