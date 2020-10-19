d95e1fabd457ebc91754a25e7f95acadd742da7b
Nick Kniola <nickkniola@me.com>
2020-10-19 13:32:50 -0700

    In progress

---
+++ object-manipulation-2/src/pick.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+
+function pick(source, keys) {
+  var selectedObject = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    var prop = keys[i];
+    if (source[prop]) {
+
+    }
+  }
+}
