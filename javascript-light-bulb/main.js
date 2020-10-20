31d1bcdfe324a0f632a5b533fa281116e03819db
Nick Kniola <nickkniola@me.com>
2020-10-20 11:46:51 -0700

    Switched data source from DOM to boolean

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,13 +1,16 @@
+var switchOn = false;
 var lightBulb = document.querySelector('.light-bulb');
 var container = document.querySelector('.container');
 lightBulb.addEventListener('click', flipLightSwitch);
 
 function flipLightSwitch() {
-  if (lightBulb.className === 'light-bulb') {
+  if (!switchOn) {
     lightBulb.className = lightBulb.className + ' light-bulb-on';
     container.className = container.className + ' container-on';
+    switchOn = true;
   } else {
     lightBulb.className = 'light-bulb';
     container.className = 'container';
+    switchOn = false;
   }
 }
