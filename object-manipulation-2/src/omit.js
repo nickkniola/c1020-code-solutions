585ef82627aea0a573535b95cba1f530e3909162
Nick Kniola <nickkniola@me.com>
2020-10-19 17:19:44 -0700

    Completed Object Manipulation 2

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,14 @@
 /* eslint-disable no-unused-vars */
+
+function omit(source, keys) {
+  var notListedObject = {};
+  var sourceKeys = Object.keys(source);
+
+  for (var i = 0; i < sourceKeys.length; i++) {
+
+    if (!keys.includes(sourceKeys[i])) {
+      notListedObject[sourceKeys[i]] = source[sourceKeys[i]];
+    }
+  }
+  return notListedObject;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -5,8 +5,10 @@ function pick(source, keys) {
 
   for (let i = 0; i < keys.length; i++) {
     var prop = keys[i];
-    if (source[prop]) {
-
+    if (source[prop] || source[prop] === null || source[prop] === 0) {
+      selectedObject[prop] = source[prop];
     }
   }
+
+  return selectedObject;
 }
