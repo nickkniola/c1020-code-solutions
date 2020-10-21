01a11a4ca51356cf78305a4384813c8a01fe5cac
Nick Kniola <nickkniola@me.com>
2020-10-21 16:32:00 -0700

    swapChars in progress

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,9 @@
 /* eslint-disable no-unused-vars */
+
+function lastChars(length, string) {
+  if (length > string.length) {
+    return string;
+  }
+
+  return string.slice(string.length - length);
+}
--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,6 @@
 /* eslint-disable no-unused-vars */
+function swapChars(firstIndex, secondIndex, string) {
+  var firstLetter = string.slice(firstIndex, firstIndex + 1);
+  var secondLetter = string.slice(secondIndex, secondIndex + 1);
+
+}
