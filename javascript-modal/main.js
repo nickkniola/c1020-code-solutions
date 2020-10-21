d2b3bdf9a01ebc13f2ccd5b00d7f916c5fa48ed5
Nick Kniola <nickkniola@me.com>
2020-10-20 17:44:02 -0700

    Revised modal, centering, visibility

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -6,24 +6,21 @@ var closeModalButton = document.querySelector('.close-modal-button');
 closeModalButton.addEventListener('click', closeModal);
 
 var modal = document.querySelector('.modal');
-var overlay = document.querySelector('.overlay');
 
 var container = document.querySelector('.container');
 
 function openModal() {
   if (modalOpen === false) {
-    modal.className = 'modal visible';
-    overlay.className = 'overlay visible';
-    container.className = 'container visible';
+    modal.className = 'modal';
+    container.className = 'container';
   }
   modalOpen = true;
 }
 
 function closeModal() {
   if (modalOpen === true) {
-    modal.className = 'modal';
-    overlay.className = 'overlay';
-    container.className = 'container';
+    modal.className = 'modal invisible';
+    container.className = 'container invisible';
   }
   modalOpen = false;
 }
