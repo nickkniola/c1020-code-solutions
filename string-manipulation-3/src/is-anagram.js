48caccbf63ae455a427fd10015a20cc9a1f6b79e
Nick Kniola <nickkniola@me.com>
2020-10-22 13:02:51 -0700

    is anagram in progress

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,13 +1,18 @@
 /* eslint-disable no-unused-vars */
 function isAnagram(firstString, secondString) {
-  secondString = secondString.replace(' ', '');
+  secondString = secondString.replaceAll(' ', '');
 
   for (var i = 0; i < secondString.length; i++) {
     var secondStringChar = secondString[i];
 
     if (!firstString.includes(secondStringChar)) {
       return false;
+    } else {
+      firstString = firstString.replace(secondStringChar, '');
+      secondString = secondString.replace(secondStringChar, '');
     }
   }
   return true;
 }
+
+/* console.log(isAnagram('ludicrous', 'ridiculous')); */
