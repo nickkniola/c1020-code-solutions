6b871722105746bf57d6fd41ec63c7d448b3de58
Nick Kniola <nickkniola@me.com>
2020-11-01 17:20:13 -0800

    Finished Difference Function

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,9 +1,28 @@
 /* eslint-disable no-unused-vars */
 
 function difference(first, second) {
-  // create uniqueArray
-
+  // create empty array uniqueArray
+  var uniqueArray = [];
   // loop through first array
-  // access element of first/second arrays
-  //
+  for (var i = 0; i < first.length; i++) {
+  // have a boolean set to true
+    var isUnique = true;
+    // access element of first array
+    var firstElement = first[i];
+    // loop through second array
+    for (var j = 0; j < second.length; j++) {
+      // access element of 2nd array
+      var secondElement = second[i];
+      // see if elements are the same, if so, set boolean to false
+      if (firstElement === secondElement) {
+        isUnique = false;
+      }
+    }
+    // outside the 2nd array loop, push element from 1st array to uniqueArray
+    if (isUnique) {
+      uniqueArray.push(firstElement);
+    }
+  }
+  // return uniqueArray
+  return uniqueArray;
 }
