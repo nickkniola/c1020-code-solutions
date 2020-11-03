eb6805ad82cb71a767e1897915dc494e9b2c93f7
Nick Kniola <nickkniola@me.com>
2020-11-02 18:02:03 -0800

    Added functionality and started points scoring

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,9 +1,31 @@
 var allSpanElements = document.querySelectorAll('span');
 var i = 0;
+var attempts = 0;
+var total = 0;
 
 document.addEventListener('keydown', function (event) {
-  console.log(event.key);
+  if (i === allSpanElements.length - 1) {
+    scorePoints();
+  }
   if (allSpanElements[i].textContent === event.key) {
     i++;
+    // change word color to green
+    allSpanElements[i].className = 'selected';
+    allSpanElements[i - 1].className = 'correct';
+    accuracy(1);
+  } else {
+    // change word color to red
+    allSpanElements[i].className = 'incorrect';
+    accuracy(-1);
   }
 });
+
+function accuracy(point) {
+  attempts++;
+  total = total + point;
+}
+
+function scorePoints() {
+  var score = attempts / total;
+  return score;
+}
