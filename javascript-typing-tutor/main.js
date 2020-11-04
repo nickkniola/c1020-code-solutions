325d12d2cdbb13ce31f47568659b3bb448f3578c
Nick Kniola <nickkniola@me.com>
2020-11-03 17:43:34 -0800

    Completed JavaScript Typing Tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -3,9 +3,12 @@ var i = 0;
 var attempts = 0;
 var total = 0;
 
+var playAgainButton = document.querySelector('button');
+
 document.addEventListener('keydown', function (event) {
-  if (i === allSpanElements.length - 1) {
+  if (i === allSpanElements.length - 2) {
     scorePoints();
+    playAgainButton.className = 'visible';
   }
   if (allSpanElements[i].textContent === event.key) {
     i++;
@@ -25,7 +28,25 @@ function accuracy(point) {
   total = total + point;
 }
 
+var scoreh2 = document.querySelector('h2.score');
+
 function scorePoints() {
-  var score = attempts / total;
-  return score;
+  var score = Math.round((total / attempts) * 100);
+  if (score < 0) {
+    score = 0;
+  }
+  scoreh2.textContent = 'Accuracy Score: ' + score + '%';
+}
+
+playAgainButton.addEventListener('click', playAgain);
+
+function playAgain() {
+  for (var j = 0; j < allSpanElements.length; j++) {
+    allSpanElements[j].className = '';
+  }
+  i = 0;
+  attempts = 0;
+  total = 0;
+  scoreh2.textContent = '';
+  playAgainButton.className = 'invisible';
 }
