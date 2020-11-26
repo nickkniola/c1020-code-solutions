29f796d8f63ec5dbe8d01efd87c52b722b293266
Nicholas Kniola <nickkniola@me.com>
2020-11-26 02:31:36 +0000

    Completed Level 3 of Node FS Readfile

---
+++ node-fs-readfile/cat.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const fileNames = process.argv.splice(2);
+let textContent = '';
+
+function filesReader(i = 0) {
+  fs.readFile(fileNames[i], 'utf8', (err, data) => {
+    if (err) throw err;
+    textContent += data + '\n';
+    if (i + 1 === fileNames.length) {
+      console.log(textContent);
+      return;
+    }
+    return setTimeout(function () { filesReader(i++); }, 500, i++);
+  });
+}
+filesReader();
