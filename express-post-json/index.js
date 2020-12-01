28f0a6b90b325e166bc75dab8f8773808ebd59f8
Nicholas Kniola <nickkniola@me.com>
2020-12-01 20:07:44 +0000

    Complete Express Post Json

---
+++ express-post-json/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const grades = [];
+let nextId = 1;
+
+app.use(express.json()); // JSON request body parsing (to object) middleware
+
+app.get('/api/grades', (req, res) => {
+  res.json(grades);
+});
+
+app.post('/api/grades', (req, res) => {
+  grades.push(req.body);
+  grades[grades.length - 1].id = nextId;
+  nextId++;
+  res.status(201).json(grades);
+});
+
+app.listen(3000, () => {
+  process.stdout.write('Server Running on Port 3000');
+});
