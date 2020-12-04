b312d216a9bcda0eff53921313bb0140b2158ae3
Nicholas Kniola <nickkniola@me.com>
2020-12-04 23:32:55 +0000

    Add post method to app object with err handling

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const pg = require('pg');
-
+const jsonParse = express.json();
 const db = new pg.Pool({
   connectionString: 'postgres://dev:lfz@localhost/studentGradeTable'
 });
 
-app.listen(port, () => {
-  process.stdout.write(`Listening at Port: ${port}`);
-});
+app.use(jsonParse);
 
 app.get('/api/grades', (req, res, next) => {
   const sql = `
@@ -27,4 +25,45 @@ app.get('/api/grades', (req, res, next) => {
         error: 'An unexpected error occurred.'
       });
     });
+});
+
+app.post('/api/grades', (req, res, next) => {
+  const name = req.body.name;
+  const course = req.body.course;
+  const score = parseInt(req.body.score);
+  const sql = `
+    insert into "grades" ("name", "course", "score")
+    values ($1, $2, $3)
+    returning *;
+  `;
+  const values = [name, course, score];
+  if (!Number.isInteger(score) || score < 0 || score > 100) {
+    res.status(400).json({
+      error: 'Score must be a positive integer between 0 and 100'
+    });
+  } else if (!name) {
+    res.status(400).json({
+      error: 'Missing name'
+    });
+  } else if (!course) {
+    res.status(400).json({
+      error: 'Missing course'
+    });
+  } else {
+    db.query(sql, values)
+      .then(result => {
+        const grade = result.rows[0];
+        res.status(201).json(grade);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({
+          error: 'An unexpected error occurred'
+        });
+      });
+  }
+});
+
+app.listen(port, () => {
+  process.stdout.write(`Listening at Port: ${port}`);
 });
