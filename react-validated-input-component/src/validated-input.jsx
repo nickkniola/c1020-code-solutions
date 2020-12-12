2d83573e05269048a5b004af88dd4487da98dcec
Nicholas Kniola <nickkniola@me.com>
2020-12-12 02:06:23 +0000

    Form password input created

---
+++ react-validated-input-component/src/validated-input.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class ValidatedInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: ''
+    };
+    this.handlePassword = this.handlePassword.bind(this);
+  }
+
+  handlePassword() {
+    this.setState({
+      password: this.state.password
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label htmlFor="password">Password</label>
+          <br></br>
+          <input type="password" name="password" onChange={this.handlePassword}></input>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ValidatedInput;
