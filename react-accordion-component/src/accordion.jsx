5c24110c26c12da6d0649002bc157aa0a2808589
Nicholas Kniola <nickkniola@me.com>
2020-12-15 23:03:38 +0000

    Add toggleAccordion event handler

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 
 class Accordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openAccordionId: 1
+    };
+    this.toggleAccordion = this.toggleAccordion.bind(this);
+  }
+
+  toggleAccordion(event) {
+    // if it's not an integer
+    if (!this.state.openAccordionId.isInteger()) {
+      // console.log('not an integer');
+    } else {
+      // console.log.log(event.target);
+    }
+  }
+
   render() {
     const arrayOfData = [{
       id: 1,
@@ -21,7 +38,7 @@ class Accordion extends React.Component {
     return (
       <div className="accordion">
         {arrayOfData.map(obj =>
-          <Topic key={obj.id} header={obj.header} body={obj.body} />
+          <Topic key={obj.id} header={obj.header} body={obj.body} handleClick={this.toggleAccordion}/>
         )}
       </div>
     );
@@ -32,7 +49,7 @@ class Topic extends React.Component {
   render() {
     return (
       <div className="topic">
-        <div className="header">
+        <div className="header" onClick={this.props.handleClick}>
           <h3>{this.props.header}</h3>
         </div>
         <div className="body">
