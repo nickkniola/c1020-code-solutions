424ac5fb0ff06d1d0fa06d004e1730286e15ca93
Nicholas Kniola <nickkniola@me.com>
2020-12-15 01:28:07 +0000

    Complete Fetch In React

---
+++ fetch-in-react/src/app.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import UserList from './user-list';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(dataArray => this.setState({ users: dataArray }))
+      .catch(err => console.error(err))
+      .finally(this.setState({ isLoading: false }));
+  }
+
+  render() {
+    return this.state.isLoading
+      ? <p>Loading...</p>
+      : <UserList users={this.state.users} />;
+  }
+}
+
+export default App;
