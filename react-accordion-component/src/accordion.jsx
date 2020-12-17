f006d6640d22ce01fafaf8c304eacf3191773ece
Nicholas Kniola <nickkniola@me.com>
2020-12-17 05:33:53 +0000

    Change data location from state to props

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -4,21 +4,6 @@ class Accordion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      arrayOfData: [{
-        id: 1,
-        header: 'JavaScript',
-        body: 'JavaScript (/ˈdʒɑːvəˌskrɪpt/),[6] often abbreviated as JS, is a programming language that conforms to the ECMAScript specification.[7] JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.'
-      },
-      {
-        id: 2,
-        header: 'HTML',
-        body: 'Hypertext Markup Language (HTML) is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.'
-      },
-      {
-        id: 3,
-        header: 'CSS',
-        body: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.[1] CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.[2]'
-      }],
       openAccordionId: null
     };
     this.toggleAccordion = this.toggleAccordion.bind(this);
@@ -26,7 +11,7 @@ class Accordion extends React.Component {
 
   toggleAccordion(event) {
     const prevAccordionId = this.state.openAccordionId;
-    const element = this.state.arrayOfData.filter(obj => {
+    const element = this.props.arrayOfData.filter(obj => {
       return event.target.textContent === obj.header;
     });
 
@@ -38,7 +23,7 @@ class Accordion extends React.Component {
   }
 
   render() {
-    const arrayOfData = this.state.arrayOfData;
+    const arrayOfData = this.props.arrayOfData;
     const id = this.state.openAccordionId;
     return (
       <div className="accordion">
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -2,4 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Accordion from './accordion';
 
-ReactDOM.render(<Accordion />, document.querySelector('#root'));
+const accordionData = [{
+  id: 1,
+  header: 'JavaScript',
+  body: 'JavaScript (/ˈdʒɑːvəˌskrɪpt/),[6] often abbreviated as JS, is a programming language that conforms to the ECMAScript specification.[7] JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.'
+},
+{
+  id: 2,
+  header: 'HTML',
+  body: 'Hypertext Markup Language (HTML) is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.'
+},
+{
+  id: 3,
+  header: 'CSS',
+  body: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.[1] CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.[2]'
+}];
+
+ReactDOM.render(<Accordion arrayOfData={accordionData}/>, document.querySelector('#root'));
