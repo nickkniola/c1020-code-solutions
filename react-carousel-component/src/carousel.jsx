716cdf7f1da0601204dde5595c2a1b89220da259
Nicholas Kniola <nickkniola@me.com>
2020-12-17 06:09:35 +0000

    Style the carousel

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -11,16 +11,28 @@ export default class Carousel extends React.Component {
   render() {
     const image = this.props.images[this.state.currentImageIndex];
     return (
-      <div className="carousel container">
-        <div className="row">
-          <div className="left-arrow"></div>
-          <div className="image">
-            <img src={image}/>
+      <div className="section">
+        <div className="carousel container">
+          <div className="main-row">
+            <div className="left-arrow">
+              <i className="fas fa-chevron-left"></i>
+            </div>
+            <div className="column">
+              <div className="image">
+                <img src={image}/>
+              </div>
+              <div className="dots">
+                <i className="fas fa-circle circ"></i>
+                <i className="far fa-circle circ"></i>
+                <i className="far fa-circle circ"></i>
+                <i className="far fa-circle circ"></i>
+                <i className="far fa-circle circ"></i>
+              </div>
+            </div>
+            <div className="right-arrow">
+              <i className="fas fa-chevron-right"></i>
+            </div>
           </div>
-          <div className="rightarrow"></div>
-        </div>
-        <div className="row">
-          <div className="dots"></div>
         </div>
       </div>
     );
